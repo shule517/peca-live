9d4d82996d29ca42d46ae0f5eacc6e91c7aba3d3
shule517 <shule517@gmail.com>
2019-09-15 00:33:49 +0900

    APIをクロスドメイン対応

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module Pecalive
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+                 headers: :any,
+                 methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
