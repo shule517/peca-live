b17f71dcd1574bd2175779e5b0f32e6529fb86ca
shule517 <shule517@gmail.com>
2019-09-15 01:10:37 +0900

    YP情報をキャッシュする(1分間したら再取得)

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::ChannelsController < ApplicationController
   def index
-    tp_response = get("http://temp.orz.hm/yp/index.txt")
-    sp_response = get("http://bayonet.ddo.jp/sp/index.txt")
-    render json: (tp_response + sp_response)
+    render json: get_channels
   end
 
   private
+
+  def get_channels
+    Rails.cache.fetch('get_channels', expires_in: 1.minute) do
+      tp_response = get("http://temp.orz.hm/yp/index.txt")
+      sp_response = get("http://bayonet.ddo.jp/sp/index.txt")
+      tp_response + sp_response
+    end
+  end
 
   def get(url)
     uri = URI.parse(url)
