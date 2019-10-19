143fa30b7d1a1c6b01ab7291c3e45b8cf7c73b4a
shule517 <shule517@gmail.com>
2019-10-19 20:19:56 +0900

    ReactRouterを導入

--- app/javascript/packs/app.tsx
+++ app/javascript/packs/app.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const ChannelList = () => {
+  return (
+    <div>
+      チャンネル一覧
+    </div>
+  )
+}
+
+const ChannelPlayer = () => {
+  return (
+    <div>
+      チャンネル再生
+    </div>
+  )
+}
 
 const App = () => {
-    return (
-        <div>
-            test
-        </div>
-    )
+  return (
+    <BrowserRouter>
+      <Link to='/'>チャンネル一覧</Link>
+      <Link to='/channels/1234'>チャンネル再生</Link>
+      <Route exact path='/' render={(props) => <ChannelList />} />
+      <Route path='/channels/:streamId' render={(props) => { return <ChannelPlayer />}} />
+    </BrowserRouter>
+  )
 }
 
 export default App;
