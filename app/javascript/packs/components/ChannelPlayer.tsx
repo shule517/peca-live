38f9302249593aae1c321e347ba5ea025eba2e25
shule517 <shule517@gmail.com>
2019-10-26 07:30:21 +0900

    リファクタリング：Videoコンポーネントを切り出した

--- app/javascript/packs/components/ChannelPlayer.tsx
+++ app/javascript/packs/components/ChannelPlayer.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
-import flvjs from 'flv.js'
-import videojs from 'video.js'
 import styled from 'styled-components';
 import Channel from '../types/Channel';
 import { Helmet } from 'react-helmet';
+import Video from './Video'
 
 type Props = {
   streamId: String,
@@ -56,43 +55,7 @@ const ChannelPlayer = (props: Props) => {
     return text;
   };
 
-  const videoElementId = `videoElement-${channel.streamId}`;
-  let flvPlayer: any = null;
-  let hiddenPlayer = true;
-
   window.scrollTo(0, 0);
-
-  const streamUrl = `http://150.95.177.111:7144/pls/${channel.streamId}.m3u8?tip=${channel.tip}&fmt=m3u8`;
-  const isHlsPlay = isHls && channel.streamId.length;
-
-  useEffect(() => {
-    // TODO HLS再生
-    if (isHlsPlay) {
-      var player = videojs(videoElementId);
-      return;
-    }
-
-    if (isHls) {
-      return;
-    }
-
-    // TODO FLV再生
-    let videoElement:any = document.getElementById(videoElementId);
-    videoElement.hidden = false;
-
-    if (channel.streamId.length > 0) {
-      const url = `http://150.95.177.111:7144/stream/${channel.streamId}.flv?tip=${channel.tip}`;
-      flvPlayer = flvjs.createPlayer({
-        type: 'flv',
-        isLive: true,
-        url: url
-      });
-      flvPlayer.attachMediaElement(videoElement);
-      flvPlayer.load();
-      flvPlayer.play();
-      hiddenPlayer = false;
-    }
-  });
 
   console.log('channel.streamId: ' + channel.streamId);
   console.log('channel.streamId.length: ' + channel.streamId.length)
@@ -101,14 +64,7 @@ const ChannelPlayer = (props: Props) => {
     <ChannelItemStyle>
       <Helmet title={`${channel.name} - ぺからいぶ！`} />
       <div>
-        {isHls ? null : <video id={videoElementId} controls width="100%"></video> }
-        {
-          isHlsPlay ? (
-            <video id={videoElementId} width={1280} height={720} className="video-js vjs-default-skin" controls >
-              <source src={streamUrl} type="application/x-mpegURL" />
-            </video>
-          ) : null
-        }
+        <Video channel={channel} isHls={isHls} />
       </div>
       <ChannelDetail>
         <Title>
---
+++ app/javascript/packs/components/Video.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import flvjs from 'flv.js'
+import videojs from 'video.js'
+import Channel from '../types/Channel';
+
+type Props = {
+  channel: Channel,
+  isHls: boolean,
+}
+
+const Video = (props: Props) => {
+  const {
+    channel,
+    isHls,
+  } = props;
+
+  const videoElementId = `videoElement-${channel.streamId}`;
+  let flvPlayer: any = null;
+  let hiddenPlayer = true;
+
+  const streamUrl = `http://150.95.177.111:7144/pls/${channel.streamId}.m3u8?tip=${channel.tip}&fmt=m3u8`;
+  const isHlsPlay = isHls && channel.streamId.length;
+
+  useEffect(() => {
+    // TODO HLS再生
+    if (isHlsPlay) {
+      var player = videojs(videoElementId);
+      return;
+    }
+
+    if (isHls) {
+      return;
+    }
+
+    // TODO FLV再生
+    let videoElement:any = document.getElementById(videoElementId);
+    videoElement.hidden = false;
+
+    if (channel.streamId.length > 0) {
+      const url = `http://150.95.177.111:7144/stream/${channel.streamId}.flv?tip=${channel.tip}`;
+      flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        isLive: true,
+        url: url
+      });
+      flvPlayer.attachMediaElement(videoElement);
+      flvPlayer.load();
+      flvPlayer.play();
+      hiddenPlayer = false;
+    }
+  });
+
+  return (
+    <div>
+      {isHls ? null : <video id={videoElementId} controls width="100%"></video> }
+      {
+        isHlsPlay ? (
+          <video id={videoElementId} width={1280} height={720} className="video-js vjs-default-skin" controls >
+            <source src={streamUrl} type="application/x-mpegURL" />
+          </video>
+        ) : null
+      }
+    </div>
+  );
+};
+
+export default Video;
