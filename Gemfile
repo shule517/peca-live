6da8957ce7cef3c849ce3bebc6f2452b7176496e
shule517 <shule517@gmail.com>
2019-10-27 20:03:58 +0900

    PeerCastStationのAPIのリクエスト時にBASIC認証する

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'dotenv-rails'
 end
 
 group :test do
--- lib/json_rpc.rb
+++ lib/json_rpc.rb
@@ -1,6 +1,7 @@
 class JsonRpc
-  def initialize(entry_point)
+  def initialize(entry_point, basic_token)
     @entry_point = entry_point
+    @basic_token = basic_token
   end
 
   def update_yp_channels
@@ -11,9 +12,9 @@ class JsonRpc
   private
 
   def command(method_name)
-    result = `curl -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X POST -d '{"jsonrpc": "2.0","id": 6412,"method":"#{method_name}"}' #{entry_point}`
+    result = `curl -H "Authorization: Basic #{basic_token}" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X POST -d '{"jsonrpc": "2.0","id": 6412,"method":"#{method_name}"}' #{entry_point}`
     JSON.parse(result)
   end
 
-  attr_reader :entry_point
+  attr_reader :entry_point, :basic_token
 end
