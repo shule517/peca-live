08a9b8b2436a80fd1df40ff8faf4769e14da46aa
shule517 <shule517@gmail.com>
2019-10-27 13:26:08 +0900

    非公開チャンネルを設定

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -10,8 +10,19 @@ class Api::V1::ChannelsController < ApplicationController
       peca_tip = 'http://150.95.177.111:7144'
       api = JsonRpc.new("#{peca_tip}/api/1")
       channels = api.update_yp_channels
-      channels = channels.select { |channel| channel['channelId'] != '00000000000000000000000000000000' && channel['contentType'] == 'FLV' }
+      channels = channels.select { |channel| visible_channel?(channel) }
       channels
     end
+  end
+
+  def visible_channel?(channel)
+    return false if channel['channelId'] == '00000000000000000000000000000000'
+    return false if channel['contentType'] == 'FLV'
+    return false if ignore_channel?(channel['name'])
+    true
+  end
+
+  def ignore_channel?(channel_name)
+    %w(isuZuﾋﾟﾁｭｰﾝch).include?(channel_name)
   end
 end
