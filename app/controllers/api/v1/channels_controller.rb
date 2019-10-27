0563631651741ac3614d5e521fda0e70d287effd
shule517 <shule517@gmail.com>
2019-10-27 20:21:42 +0900

    PeerCastのホスト＆ポートを環境変数に移行

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::ChannelsController < ApplicationController
 
   def get_channels
     Rails.cache.fetch('get_channels', expires_in: 1.minute) do
-      peca_tip = 'http://150.95.177.111:7144'
+      peca_tip = "http://#{ENV['PEERCAST_TIP']}"
       api = JsonRpc.new("#{peca_tip}/api/1", ENV['PEERCAST_BASIC_TOKEN'])
       channels = api.update_yp_channels
       channels = channels.select { |channel| visible_channel?(channel) }
