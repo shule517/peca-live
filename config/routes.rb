44c09696ebcc6f11c7e20f878aa7aba9bc316e27
shule517 <shule517@gmail.com>
2020-02-15 16:48:20 +0900

    IP取得のおためし

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :channels do
+        collection do
+          get :ip
+        end
       end
     end
   end
