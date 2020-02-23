64c79a3c5bae230bbb574bf74347aee9d0013346
shule517 <shule517@gmail.com>
2020-02-24 08:47:15 +0900

    annotateを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :integer          not null, primary key
+#  display_name :string           not null
+#  photo_url    :text             not null
+#  uid          :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class User < ApplicationRecord
 end
