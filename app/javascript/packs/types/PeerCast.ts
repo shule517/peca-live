233a1db808cf917659a5a38a7a3a44fd8d28e1e0
shule517 <shule517@gmail.com>
2020-07-14 06:55:02 +0900

    IPの変更

--- app/javascript/packs/types/PeerCast.ts
+++ app/javascript/packs/types/PeerCast.ts
@@ -4,7 +4,7 @@ export type PeerCastInterface = {
 }
 
 class PeerCast {
-  static defaultHost = '183.77.38.30' //'shule.peca.live'
+  static defaultHost = '183.77.31.192' //'shule.peca.live'
   static defaultPortNo = 8144
 
   constructor(public json: PeerCastInterface) {}
