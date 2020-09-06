c5c62075ea632f8bb0b84de36d0eb90f65b5af6c
shule517 <shule517@gmail.com>
2020-09-06 20:06:13 +0900

    動画コントロールのカスタマイズ

--- app/javascript/packs/components/Video.tsx
+++ app/javascript/packs/components/Video.tsx
@@ -4,6 +4,23 @@ import videojs from 'video.js'
 import Channel from '../types/Channel'
 import styled from 'styled-components'
 import { useSelectorPeerCast } from '../modules/peercastModule'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+import PictureInPictureAltIcon from '@material-ui/icons/PictureInPictureAlt'
+import FullscreenIcon from '@material-ui/icons/Fullscreen'
+import IconButton from '@material-ui/core/IconButton'
+import SettingsIcon from '@material-ui/icons/Settings'
+import Toolbar from '@material-ui/core/Toolbar'
+import RefreshIcon from '@material-ui/icons/Refresh'
+import { Tooltip } from '@material-ui/core'
+import PauseIcon from '@material-ui/icons/Pause'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import { VolumeDown, VolumeUp } from '@material-ui/icons'
+import Slider from '@material-ui/core/Slider'
+import VolumeUpIcon from '@material-ui/icons/VolumeUp'
+import VolumeOffIcon from '@material-ui/icons/VolumeOff'
 
 type Props = {
   channel: Channel
@@ -19,6 +36,7 @@ const Video = (props: Props) => {
   const [currentStreamUrl, setCurrentStreamUrl] = useState<string>(null)
   const [movieWidth, setMovieWidth] = useState<number>(1280)
   const [movieHeight, setMovieHeight] = useState<number>(720)
+  const [muted, setMuted] = useState<boolean>(false)
 
   const videoElementId = `videoElement-${channel.streamId}`
   const isHlsPlay = isHls && channel.streamId.length
@@ -80,7 +98,7 @@ const Video = (props: Props) => {
   const height = width * aspectRate
 
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
       {isHls ? null : (
         <VideoStyle
           id={videoElementId}
@@ -88,6 +106,105 @@ const Video = (props: Props) => {
           style={{ width: width, height: height }}
         />
       )}
+
+      <VideoControl style={{ width: width }}>
+        {/*<PlayButtonControl style={{ width: width, height: height }}>*/}
+        {/*  <ArrowBackIcon style={{ fontSize: 80 }} />*/}
+        {/*  <PlayArrowIcon style={{ fontSize: 150, margin: '0 80px' }} />*/}
+        {/*  <ArrowForwardIcon style={{ fontSize: 80 }} />*/}
+        {/*</PlayButtonControl>*/}
+
+        {/*<Typography id="continuous-slider" gutterBottom>*/}
+        {/*  Volume*/}
+        {/*</Typography>*/}
+        {/*<Grid container spacing={2}>*/}
+        {/*  <Grid item>*/}
+        {/*    <VolumeDown />*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item xs>*/}
+        {/*    <Slider*/}
+        {/*      value={80}*/}
+        {/*      onChange={(event, newValue) => {}}*/}
+        {/*      aria-labelledby="continuous-slider"*/}
+        {/*    />*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item>*/}
+        {/*    <VolumeUp />*/}
+        {/*  </Grid>*/}
+        {/*</Grid>*/}
+
+        <FooterControl>
+          {player ? (
+            muted ? (
+              <Tooltip title="ミュートを解除" placement="top" arrow>
+                <IconButton color="primary" component="span">
+                  <VolumeOffIcon
+                    style={{ color: 'white' }}
+                    onClick={() => {
+                      player.muted = false
+                      setMuted(player.muted)
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Tooltip title="ミュート" placement="top" arrow>
+                <IconButton color="primary" component="span">
+                  <VolumeUpIcon
+                    style={{ color: 'white' }}
+                    onClick={() => {
+                      player.muted = true
+                      setMuted(player.muted)
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+            )
+          ) : null}
+
+          <Tooltip title="再生" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <PlayArrowIcon
+                style={{ color: 'white' }}
+                onClick={() => {
+                  player.play()
+                }}
+              />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="前の配信へ" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <ArrowBackIcon style={{ color: 'white' }} />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="次の配信へ" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <ArrowForwardIcon style={{ color: 'white' }} />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="再接続" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <RefreshIcon style={{ color: 'white' }} />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="ミニプレイヤー" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <PictureInPictureAltIcon style={{ color: 'white' }} />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="フルスクリーン" placement="top" arrow>
+            <IconButton color="primary" component="span">
+              <FullscreenIcon style={{ color: 'white' }} />
+            </IconButton>
+          </Tooltip>
+        </FooterControl>
+      </VideoControl>
+
       {/*{*/}
       {/*  isHlsPlay ? (*/}
       {/*    <video id={videoElementId} width={1280} height={720} className="video-js vjs-default-skin" controls >*/}
@@ -102,6 +219,44 @@ const Video = (props: Props) => {
 const VideoStyle = styled.video`
   background-color: #333333;
   max-width: 800px;
+`
+
+const VideoControl = styled.div`
+  background: -moz-linear-gradient(
+    top,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.4) 30%,
+    rgba(0, 0, 0, 0.9)
+  );
+  background: -webkit-linear-gradient(
+    top,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.4) 30%,
+    rgba(0, 0, 0, 0.9)
+  );
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.4) 30%,
+    rgba(0, 0, 0, 0.9)
+  );
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 60px;
+  color: white;
+`
+
+const PlayButtonControl = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const FooterControl = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
 `
 
 export default Video
