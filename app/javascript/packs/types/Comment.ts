f5f5e6ef28a7141bc653d2c4a59667edea324495
shule517 <shule517@gmail.com>
2020-09-22 09:56:29 +0900

    Commentインターフェイスを追加

---
+++ app/javascript/packs/types/Comment.ts
@@ -0,0 +1,33 @@
+export type CommentInterface = {
+  no: string
+  name: string
+  mail: string
+  writed_at: string
+  body: string
+}
+
+class Comment {
+  constructor(public json: CommentInterface) {}
+
+  get no() {
+    return this.json.no
+  }
+
+  get name() {
+    return this.json.name
+  }
+
+  get mail() {
+    return this.json.mail
+  }
+
+  get writed_at() {
+    return this.json.writed_at
+  }
+
+  get body() {
+    return this.json.body
+  }
+}
+
+export default Comment
