ab235ca55cd53a489f2baa387997626e207e33f4
shule517 <shule517@gmail.com>
2020-10-12 09:16:05 +0900

    配信履歴を保存するメソッドを追加

--- app/models/channel_history.rb
+++ app/models/channel_history.rb
@@ -24,5 +24,51 @@
 #  updated_at      :datetime         not null
 #  stream_id       :string           not null
 #
+# Indexes
+#
+#  index_channel_histories_on_stream_id  (stream_id) UNIQUE
+#
 class ChannelHistory < ApplicationRecord
+  validates :stream_id, uniqueness: true, presence: true
+  validates :name, presence: true
+  validates :listeners, presence: true
+  validates :relays, presence: true
+  validates :uptime, presence: true
+  validates :latest_lived_at, presence: true
+  validates :yellow_page, presence: true
+
+  class << self
+    def record_channels(channels)
+      channels.each { |channel| record_channel(channel) }
+    end
+
+    def record_channel(channel)
+      hash = {
+        stream_id: channel['channelId'],
+        name: channel['name'],
+        yellow_page: channel['yellowPage'],
+        tracker: channel['tracker'],
+        contact_url: channel['contactUrl'],
+        genre: channel['genre'],
+        description: channel['description'],
+        comment: channel['comment'],
+        bitrate: channel['bitrate'],
+        content_type: channel['contentType'],
+        track_title: channel['trackTitle'],
+        album: channel['album'],
+        creator: channel['creator'],
+        track_url: channel['trackUrl'],
+        listeners: channel['listeners'],
+        relays: channel['relays'],
+        uptime: channel['uptime'],
+        latest_lived_at: Time.zone.now
+      }
+      history = find_by(stream_id: channel['channelId'])
+      if history.present?
+        history.update!(hash)
+      else
+        create!(hash)
+      end
+    end
+  end
 end
