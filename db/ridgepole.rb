dd521165bd7e56b136fd3b0fbfc6f9563c93d587
shule517 <shule517@gmail.com>
2020-10-15 06:13:29 +0900

    PrivateChannelのstatusにindexを追加

--- db/ridgepole.rb
+++ db/ridgepole.rb
@@ -19,6 +19,7 @@ create_table :private_channels, force: :cascade do |t|
   t.timestamps
 end
 add_index :private_channels, :name, unique: true
+add_index :private_channels, :status
 
 create_table :favorites, force: :cascade do |t|
   t.string   :user_id, null: false
