08b50c616b4a7bd6a10fb4f1c3528363128e7cc5
シュール <shule517@gmail.com>
2022-11-22 06:48:34 +0900

    Update PeerCast.ts

--- app/javascript/packs/types/PeerCast.ts
+++ app/javascript/packs/types/PeerCast.ts
@@ -4,7 +4,7 @@ export type PeerCastInterface = {
 }
 
 class PeerCast {
-  static defaultHost = '131.129.161.90' //'shule.peca.live'
+  static defaultHost = '183.77.82.43' //'shule.peca.live'
   static defaultPortNo = 8144
 
   constructor(public json: PeerCastInterface) {}
